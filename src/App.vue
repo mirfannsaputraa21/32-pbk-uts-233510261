ae420083a51eea24c5dee6b0648a3c17d96111c8
mirfannsaputraa21 <m.irfansaputra0089@gmail.com>
2025-04-26 13:42:56 +0700

    membuat todolist kegiatan

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <script setup>
+import { ref, computed, onMounted, watch } from 'vue';
+
+const kegiatan = ref([
+  { nomor: 1, Text: 'praktikum pemograman berbasis komponen', done: true },
+  { nomor: 2, Text: 'praktikum pemograman berorientasi objek', done: true },
+  { nomor: 3, Text: 'matematika deskrit', done: false }
+]);
 
 </script>
 
